7ab7d4747b0788e11c136d501e9c8ea48d8946b6
Kacpero60 <kapiw2002@gmail.com>
2024-10-23 16:18:53 +0200

    vite header footer

---
+++ src/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    // Load header content
+    $('#header').load('/src/partials/header.html');
+  
+    // Load about-us section content
+    $('#about-us').load('/src/partials/about-us.html');
+  
+    // Load footer content
+    $('#footer').load('/src/partials/footer.html');
+});
