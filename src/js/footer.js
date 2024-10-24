ffa61e9ca64c76a5152ed7ed3856ddb3adc12264
Kacpero60 <kapiw2002@gmail.com>
2024-10-24 16:04:29 +0200

    hero gotowe

--- src/js/footer.js
+++ src/js/footer.js
@@ -15,4 +15,20 @@ document.addEventListener("DOMContentLoaded", function () {
       });
     });
   });
+
+  // header.js
+$(document).ready(function() {
+  $('#header').load('/src/partials/header.html');
+});
+
+// hero.js
+$(document).ready(function() {
+  $('#hero').load('/src/partials/hero.html');
+});
+
+// footer.js
+$(document).ready(function() {
+  $('#footer').load('/src/partials/footer.html');
+});
+
   
--- src/js/hero.js
+++ src/js/hero.js
@@ -12,3 +12,9 @@ $(document).ready(function() {
 $(document).ready(function() {
     $('#footer').load('/src/partials/footer.html');
 });
+
+$(window).on("load", function() {
+    $('#hero').load('/src/partials/hero.html', function() {
+        // Możesz tu wywołać funkcje, które są potrzebne po załadowaniu hero
+    });
+});
