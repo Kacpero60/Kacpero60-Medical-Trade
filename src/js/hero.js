c149ce9366fdb3e2e0430dc48e7d2172272ff2ed
Kacpero60 <kapiw2002@gmail.com>
2024-10-28 13:09:10 +0100

    Update hero.js

--- src/js/hero.js
+++ src/js/hero.js
@@ -13,20 +13,17 @@ $(document).ready(function() {
     $('#footer').load('/src/partials/footer.html');
 });
 
-$(window).on("load", function() {
-    $('#hero').load('/src/partials/hero.html', function() {
-        // Możesz tu wywołać funkcje, które są potrzebne po załadowaniu hero
+// Carousel scrolling
+document.addEventListener("DOMContentLoaded", () => {
+    const leftArrow = document.querySelector('.left-arrow');
+    const rightArrow = document.querySelector('.right-arrow');
+    const logoCarousel = document.querySelector('.logo-carousel');
+
+    leftArrow.addEventListener('click', () => {
+        logoCarousel.scrollBy({ left: -150, behavior: 'smooth' });
     });
-});
 
-const leftArrow = document.querySelector('.left-arrow');
-const rightArrow = document.querySelector('.right-arrow');
-const logoCarousel = document.querySelector('.logo-carousel');
-
-leftArrow.addEventListener('click', () => {
-    logoCarousel.scrollBy({ left: -150, behavior: 'smooth' });
-});
-
-rightArrow.addEventListener('click', () => {
-    logoCarousel.scrollBy({ left: 150, behavior: 'smooth' });
+    rightArrow.addEventListener('click', () => {
+        logoCarousel.scrollBy({ left: 150, behavior: 'smooth' });
+    });
 });
