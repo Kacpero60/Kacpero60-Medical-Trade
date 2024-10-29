c189a9b1d69e396045916c28b69eb4ebbd67740a
Kacpero60 <kapiw2002@gmail.com>
2024-10-29 17:00:50 +0100

    usuniety element z sekcji hero

--- src/js/hero.js
+++ src/js/hero.js
@@ -13,17 +13,4 @@ $(document).ready(function() {
     $('#footer').load('/src/partials/footer.html');
 });
 
-// Carousel scrolling
-document.addEventListener("DOMContentLoaded", () => {
-    const leftArrow = document.querySelector('.left-arrow');
-    const rightArrow = document.querySelector('.right-arrow');
-    const logoCarousel = document.querySelector('.logo-carousel');
 
-    leftArrow.addEventListener('click', () => {
-        logoCarousel.scrollBy({ left: -150, behavior: 'smooth' });
-    });
-
-    rightArrow.addEventListener('click', () => {
-        logoCarousel.scrollBy({ left: 150, behavior: 'smooth' });
-    });
-});
