f58f9835e8d001c44a539952968cf12c1af86cfc
Kacpero60 <kapiw2002@gmail.com>
2024-11-05 19:03:21 +0100

    gg

--- src/js/privacy_police.js
+++ src/js/privacy_police.js
@@ -1,12 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const privacyPolicyButton = document.querySelector('a[href="#privacy-policy"]');
+    
+    if (privacyPolicyButton) {
+        privacyPolicyButton.addEventListener("click", (event) => {
+            event.preventDefault(); // Zapobiega domyślnemu działaniu linku
+            openPopUp(); // Otwiera pop-up
+        });
+    }
 
-// Funkcja do zamykania okna polityki prywatności
-function closePrivacyPolicy() {
-    document.querySelector('.privacy-policy-container').style.display = 'none';
-    document.querySelector('.inquiry-container').classList.remove('inquiry-blur'); // Usunięcie rozmycia
-}
+    // Funkcja do otwierania pop-upu
+    function openPopUp() {
+        const popUpContainer = document.getElementById('popUpContainer');
+        popUpContainer.style.display = 'flex'; // Pokaż pop-up
 
-// Funkcja do wyświetlania polityki prywatności
-function showPrivacyPolicy() {
-    document.querySelector('.privacy-policy-container').style.display = 'block';
-    document.querySelector('.inquiry-container').classList.add('inquiry-blur'); // Dodanie rozmycia
-}
+        // Zamknięcie pop-upu
+        document.getElementById('closePopUp').onclick = function () {
+            popUpContainer.style.display = 'none';
+        };
+    }
+});
